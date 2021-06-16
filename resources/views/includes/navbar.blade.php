a98efc91360b86762e813ab806db045db6aca92c
rullirul8 <mystiquerull96@gmail.com>
2021-06-16 13:10:24 +0700

    membuat categories laravel

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -3,7 +3,7 @@
       data-aos="fade-down"
     >
       <div class="container">
-        <a href="/index.html" class="navbar-brand">
+        <a href="{{ route('home') }}" class="navbar-brand">
           <img src="/images/logo.svg" alt="logo" />
         </a>
         <button
@@ -16,11 +16,11 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-              <a href="/index.html" class="nav-link">Home</a>
+            <li class="nav-item">
+              <a href="{{ route('home') }}" class="nav-link">Home</a>
             </li>
             <li class="nav-item">
-              <a href="/categories.html" class="nav-link">Categories</a>
+              <a href="{{ route('categories') }}" class="nav-link">Categories</a>
             </li>
             <li class="nav-item">
               <a href="#" class="nav-link">Reward</a>
--- resources/views/pages/category.blade.php
+++ resources/views/pages/category.blade.php
@@ -1,68 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1, shrink-to-fit=no"
-    />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
+@extends('layouts.app')
 
-    <title>Store - Your Best Marketplace</title>
+@section('title')
+    Store Category Page
+@endsection
 
-    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
-    <link href="style/main.css" rel="stylesheet" />
-  </head>
+@section('content') 
 
-  <body>
-    <nav
-      class="
-        navbar navbar-expand-lg navbar-light navbar-store
-        fixed-top
-        navbar-fixed-top
-      "
-      data-aos="fade-down"
-    >
-      <div class="container">
-        <a href="/index.html" class="navbar-brand">
-          <img src="/images/logo.svg" alt="logo" />
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarResponsive"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <a href="/index.html" class="nav-link">Home</a>
-            </li>
-            <li class="nav-item active">
-              <a href="/categories.html" class="nav-link">Categories</a>
-            </li>
-            <li class="nav-item">
-              <a href="#" class="nav-link">Reward</a>
-            </li>
-            <li class="nav-item">
-              <a href="/register.html" class="nav-link">Sign Up</a>
-            </li>
-            <li class="nav-item">
-              <a
-                href="/login.html"
-                class="btn btn-success nav-link px-4 text-white"
-                >Sign In</a
-              >
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-
-    <!-- Page Content -->
     <div class="page-content page-home">
       <section class="store-trend-categories">
         <div class="container">
@@ -295,24 +238,5 @@
         </div>
       </div>
     </div>
-
-    <footer>
-      <div class="container">
-        <div class="row">
-          <div class="col-12 text-center">
-            <p class="pt-4 pb-2">2021 Copyright Store. All Right Reserved.</p>
-          </div>
-        </div>
-      </div>
-    </footer>
-
-    <!-- Bootstrap core JavaScript -->
-    <script src="/vendor/jquery/jquery.slim.min.js"></script>
-    <script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-    <script>
-      AOS.init();
-    </script>
-    <script src="/script/navbar-scroll.js"></script>
-  </body>
-</html>
+    
+@endsection
